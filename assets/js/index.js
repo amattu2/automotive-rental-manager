3cd392d05eb65cce372d2cf5a46a63834e8a67f6
Alec M <38357871+amattu2@users.noreply.github.com>
2019-12-27 22:27:56 -0500

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,28 @@
+/*
+	Produced 2019
+	By https://amattu.com/links/github
+	Copy Alec M.
+	License GNU Affero General Public License v3.0
+*/
+
+// Loops
+document.querySelectorAll('.profile .profile-item').forEach(element => {
+	// Events
+	element.onclick = (event) => {
+		event.stopPropagation();
+	}
+});
+
+// Events
+document.getElementById('profile-toggle').onclick = (event) => {
+	document.getElementById('profile-toggle').classList.toggle('active');
+};
+document.getElementById('profile-toggle').onmouseenter = (event) => {
+	// Checks
+	if (document.getElementById('profile-toggle').classList.contains("active")) { return false }
+	document.getElementById('profile-expand').textContent = document.getElementById('profile-expand').textContent == "expand_more" ? "expand_less" : "expand_more";
+};
+document.getElementById('profile-toggle').onmouseleave = (event) => {
+	if (document.getElementById('profile-toggle').classList.contains("active")) { return false }
+	document.getElementById('profile-expand').textContent = document.getElementById('profile-expand').textContent == "expand_more" ? "expand_less" : "expand_more";
+};
