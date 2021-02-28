7c23a53142be44b442aad58f04cdec950f2b0750
Alec M <alecmattu1234@gmail.com>
2021-02-28 14:43:15 -0500

    Add example onclick event to rental buttons

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,22 +7,27 @@
 
 // Loops
 document.querySelectorAll('.profile .profile-item').forEach(element => {
-	// Events
-	element.onclick = (event) => {
-		event.stopPropagation();
-	}
+  // Events
+  element.onclick = (event) => {
+    event.stopPropagation();
+  };
+});
+document.querySelectorAll('.card-actions .card-action').forEach(element => {
+  element.onclick = (event) => {
+    document.getElementById('rental-edit-popover').classList.add('active');
+  };
 });
 
 // Events
 document.getElementById('profile-toggle').onclick = (event) => {
-	document.getElementById('profile-toggle').classList.toggle('active');
+  document.getElementById('profile-toggle').classList.toggle('active');
 };
 document.getElementById('profile-toggle').onmouseenter = (event) => {
-	// Checks
-	if (document.getElementById('profile-toggle').classList.contains("active")) { return false }
-	document.getElementById('profile-expand').textContent = document.getElementById('profile-expand').textContent == "expand_more" ? "expand_less" : "expand_more";
+  // Checks
+  if (document.getElementById('profile-toggle').classList.contains("active")) { return false; }
+  document.getElementById('profile-expand').textContent = document.getElementById('profile-expand').textContent == "expand_more" ? "expand_less" : "expand_more";
 };
 document.getElementById('profile-toggle').onmouseleave = (event) => {
-	if (document.getElementById('profile-toggle').classList.contains("active")) { return false }
-	document.getElementById('profile-expand').textContent = document.getElementById('profile-expand').textContent == "expand_more" ? "expand_less" : "expand_more";
+  if (document.getElementById('profile-toggle').classList.contains("active")) { return false; }
+  document.getElementById('profile-expand').textContent = document.getElementById('profile-expand').textContent == "expand_more" ? "expand_less" : "expand_more";
 };
